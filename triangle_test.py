3d119e11dc9572820bb5b6389275d52ebdd8cb89
iroman2019 <roman.ildiko@pontsystems.eu>
2019-11-08 23:40:19 +0100

    javítás

--- triangle_test.py
+++ triangle_test.py
@@ -2,6 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver=webdriver.Chrome()
+driver.get("https://iroman2019.github.io/homework/triangles.html")
 
-a=input("Az 'a' oldal hossza: ")
-driver.find_element(By.ID, "a-input") .send_keys(a)
+a_input = driver.find_element(By.ID, "a-input");
+b_input = driver.find_element(By.ID, "b-input");
+c_input = driver.find_element(By.ID, "c-input");
+calculate_button=driver.find_element(By.ID, "calculate-button")
+
+# Egyenlő oldal teszt
+a_input.send_keys(3)
+b_input.send_keys(3)
+c_input.send_keys(3)
+calculate_button.click()
+last_li=driver.find_element(By.XPATH, "//li[last()]").text
+print(last_li)
+assert last_li=="a = 3, b = 3, c = 3: Egyenlő oldalú"
+
