b7779a943608bf5417189b16ad360c684acad98b
iroman2019 <roman.ildiko@pontsystems.eu>
2019-11-09 22:18:18 +0100

    fc-javítás

--- fcconverter.py
+++ fcconverter.py
@@ -4,15 +4,15 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://www.convertworld.com/hu/homerseklet/fahrenheit.html")
 
-driver.find_element(By.NAME, "amount").send_keys(15)
-
+fahrenheit = input("A fahrenheit értéke: ")
+driver.find_element(By.NAME, "amount").send_keys(fahrenheit)
 driver.find_element(By.CLASS_NAME, "ok").click()
 f = driver.find_element(By.ID, "result-from").text
-szoveg = driver.find_element(By.ID, "result-equals").text
+web_text = driver.find_element(By.ID, "result-equals").text
 c = driver.find_element(By.XPATH, "//*[@id='result-to']/b").text
-result = f+" "+szoveg+" "+c
+result = f + " " + web_text + " " + c
 print(result)
-assert result == "15 F egyenlő -9,44 C"
+# assert result == "15 F egyenlő -9,44 C"
 driver.save_screenshot("fc_result.png")
 
 
